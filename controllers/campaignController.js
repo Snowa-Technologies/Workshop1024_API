faf2cdf5a8c196bf344933a6031e919558fa9159
VINAY\vinay <vinay.surisetty@snowatech.com>
2024-10-15 14:56:39 +0530

    Updated addnewcampain api

--- controllers/campaignController.js
+++ controllers/campaignController.js
@@ -1,28 +1,37 @@
 const { GLOBALS_ERRORS} = require('../global/globals.js');
 require("dotenv/config");
+const shortid = require('shortid');
 const {connectToMongo, getDB} = require("../services/dbService.js");
 
-const getCampaigns = async(req, res)=>{
-    res.status('OK').json();
+const getCampaigns = async(req, res) => {
+    console.log("ok")
 }
 
 const addNewCampaign = async (req, res) => {
     const newCampaign = req.body;
 
     try {
-        // await connectToMongo();
+        await connectToMongo();
 
         const campaignCollection = getDB().collection(process.env.CAMPAGIN);
 
-        // Insert the new campaign
+        let sid = shortid.generate();
+        if (sid.length > 7) {
+            sid = sid.substring(0, 7); 
+            newCampaign.c_id = sid;
+        }
+
         newCampaign.status = 1;
+        newCampaign.start_dt = new Date(newCampaign.start_dt);
+        newCampaign.end_dt = new Date(newCampaign.end_dt);
         newCampaign.c_on = new Date();
+        newCampaign.u_on = new Date();
         const result = await campaignCollection.insertOne(newCampaign);
 
         if (result.insertedId) {
             return res.status(GLOBALS_ERRORS.HTTP_STATUS.OK).json({ 
                 success: true, 
-                message: `${newCampaign.c_nm} is added successfully` 
+                message: "Campaign is added successfully "
             });
         } else {
             return res.status(GLOBALS_ERRORS.HTTP_STATUS.BAD_REQUEST).json({ 
